666225c71954d34c4bd5b31f2c6ca2f05e4b53a9
sfan5 <sfan5@live.de>
2019-09-21 22:40:45 +0200

    for testing

--- app/src/main/java/is/xyz/mpv/MPVView.kt
+++ app/src/main/java/is/xyz/mpv/MPVView.kt
@@ -113,7 +113,8 @@ internal class MPVView(context: Context, attrs: AttributeSet) : SurfaceView(cont
         // set options
 
         MPVLib.setOptionString("vo", "gpu")
-        MPVLib.setOptionString("gpu-context", "android")
+        MPVLib.setOptionString("gpu-context", "androidvk")
+        MPVLib.setOptionString("gpu-api", "vulkan")
         MPVLib.setOptionString("hwdec", hwdec)
         MPVLib.setOptionString("hwdec-codecs", "h264,hevc,mpeg4,mpeg2video,vp8,vp9")
         MPVLib.setOptionString("ao", "opensles")
